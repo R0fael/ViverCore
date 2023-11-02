808b56ff109e8e2e1fa49983eeead37dab756b50
Roman Slobodchickov <77662857+R0fael@users.noreply.github.com>
2023-11-02 18:01:57 -0500

    Add project files.

---
+++ ViverCore/Core.cs
@@ -0,0 +1,39 @@
+﻿using Cosmos.HAL.BlockDevice.Registers;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Sys = Cosmos.System;
+
+namespace ViverCore
+{
+    public class Core
+    {
+        public string processes = "Desktop;";
+        private int counter;
+        public static void Startup()
+        {
+            Console.Clear();
+            Console.WriteLine("Running Viver Core");
+        }
+        public void Input(string input)
+        {
+            foreach(string proc in processes.Split(';'))
+            {
+
+            }
+        }
+
+        private string Desktop(string input, int counter) 
+        {
+            if(counter == 0)
+            {
+                switch (input)
+                {
+                    default: break;
+                }
+                return "";
+            }
+            return "";
+        }
+    }
+}
---
+++ ViverCore/Kernel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace ViverCore
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Core.Startup();
+        }
+
+        protected override void Run()
+        {
+        }
+    }
+}
